6bf9aa3ac631b6ac7287028a72f91adb57c6ae20
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-05-08 12:53:18 +0200

    Created a Realtor class.

---
+++ Models/Realtor.cs
@@ -0,0 +1,19 @@
+﻿namespace Models
+{
+    public class Realtor : Person
+    {
+        public int Id { get; set; }
+
+        public Realtor(int id, string firstName, string lastName, string cprNumber, string streetName, int streetNumber, string zipCode, string phoneNumber)
+            : base(firstName, lastName, cprNumber, streetName, streetNumber, zipCode, phoneNumber)
+        {
+            Id = id;
+        }
+
+        public Realtor(int id, string firstName, string lastName, string cprNumber, string streetName, int streetNumber, string? apartment, string zipCode, string phoneNumber)
+            : base(firstName, lastName, cprNumber, streetName, streetNumber, apartment, zipCode, phoneNumber)
+        {
+            Id = id;
+        }
+    }
+}
