de64535c84297eb0df3bd743ac647eb9c77e22aa
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-05-08 12:44:19 +0200

    Created an abstract Person class.

---
+++ Models/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace Models
+{
+    public abstract class Person
+    {
+        public Guid Guid { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetName { get; set; }
+        public int StreetNumber { get; set; }
+        public string? Apartment { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Person(string firstName, string lastName, string streetName, int streetNumber, string zipCode, string phoneNumber)
+            : this(firstName, lastName, streetName, streetNumber, null, zipCode, phoneNumber) { }
+
+        public Person(string firstName, string lastName, string streetName, int streetNumber, string? apartment, string zipCode, string phoneNumber)
+        {
+            Guid = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            StreetName = streetName;
+            StreetNumber = streetNumber;
+            Apartment = apartment;
+            ZipCode = zipCode;
+            PhoneNumber = phoneNumber;
+        }
+    }
+}
