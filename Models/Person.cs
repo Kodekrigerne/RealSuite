f1a38ad6d8cf4fc02b6b2563bf507ab571b22580
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-05-08 12:47:07 +0200

    Added CPR nummer to Person.

--- Models/Person.cs
+++ Models/Person.cs
@@ -5,20 +5,22 @@
         public Guid Guid { get; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string CprNumber { get; set; }
         public string StreetName { get; set; }
         public int StreetNumber { get; set; }
         public string? Apartment { get; set; }
         public string ZipCode { get; set; }
         public string PhoneNumber { get; set; }
 
-        public Person(string firstName, string lastName, string streetName, int streetNumber, string zipCode, string phoneNumber)
-            : this(firstName, lastName, streetName, streetNumber, null, zipCode, phoneNumber) { }
+        public Person(string firstName, string lastName, string cprNumber, string streetName, int streetNumber, string zipCode, string phoneNumber)
+            : this(firstName, lastName, cprNumber, streetName, streetNumber, null, zipCode, phoneNumber) { }
 
-        public Person(string firstName, string lastName, string streetName, int streetNumber, string? apartment, string zipCode, string phoneNumber)
+        public Person(string firstName, string lastName, string cprNumber, string streetName, int streetNumber, string? apartment, string zipCode, string phoneNumber)
         {
             Guid = Guid.NewGuid();
             FirstName = firstName;
             LastName = lastName;
+            CprNumber = cprNumber;
             StreetName = streetName;
             StreetNumber = streetNumber;
             Apartment = apartment;
