41efc76d241c1d7becfefbc3bfc2046d4e72ed3c
MDahlmann <dahlmann.mikkel@gmail.com>
2025-05-13 10:21:26 +0200

    Enhance validation and UI in seller management
    Updated validation logic in PersonService and AddSellerPage to use regular expressions for improved input checks. Renamed variable in SellerService for clarity. Added new UI elements and event handlers in AddSellerPage to restrict input types and improve user experience.

--- BusinessLogic/PersonService.cs
+++ BusinessLogic/PersonService.cs
@@ -1,4 +1,5 @@
-﻿using Models.DTOModels;
+﻿using System.Text.RegularExpressions;
+using Models.DTOModels;
 
 namespace BusinessLogic
 {
@@ -15,7 +16,7 @@ namespace BusinessLogic
             {
                 return false;
             }
-            if (!personDTO.CprNumber.All(char.IsDigit) && personDTO.CprNumber.Length != 10)
+            if (!Regex.IsMatch(personDTO.CprNumber, @"^(?:0[1-9]|[12]\d|3[01])(?:0[1-9]|1[0-2])(?:[0-9]{2})[0-9]{4}$"))
             {
                 return false;
             }
@@ -27,11 +28,11 @@ namespace BusinessLogic
             {
                 return false;
             }
-            if (!personDTO.ZipCode.ToString().All(char.IsDigit) && (personDTO.ZipCode < 1000 && personDTO.ZipCode > 9999))
+            if (!personDTO.ZipCode.ToString().All(char.IsDigit) || (personDTO.ZipCode < 1000 || personDTO.ZipCode > 9999))
             {
                 return false;
             }
-            if (!personDTO.PhoneNumber.All(char.IsDigit) && personDTO.PhoneNumber.Length != 8)
+            if (!personDTO.PhoneNumber.All(char.IsDigit) || personDTO.PhoneNumber.Length != 8)
             {
                 return false;
             }
