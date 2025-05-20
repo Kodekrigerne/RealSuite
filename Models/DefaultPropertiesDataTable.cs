76e26e4c13daeb137992c1a3fb31d6aac7a6b70d
Dennis Johnsen <dennisjohnsenucl@gmail.com>
2025-05-20 14:17:34 +0200

    Added fault datatable classes for when connection cannot be established.

---
+++ Models/DefaultPropertiesDataTable.cs
@@ -0,0 +1,83 @@
+﻿using System.Data;
+
+namespace Models
+{
+    public class DefaultPropertiesDataTable : DataTable
+    {
+
+        public DefaultPropertiesDataTable()
+        {
+            TableName = "Properties";
+
+            DataColumn idColumn = new DataColumn();
+            idColumn.DataType = typeof(int);
+            idColumn.ColumnName = "Id";
+            Columns.Add(idColumn);
+
+            DataColumn streetNameColumn = new DataColumn();
+            streetNameColumn.DataType = typeof(string);
+            streetNameColumn.ColumnName = "StreetName";
+            Columns.Add(streetNameColumn);
+
+            DataColumn streetNumberColumn = new DataColumn();
+            streetNumberColumn.DataType = typeof(int);
+            streetNumberColumn.ColumnName = "StreetNumber";
+            Columns.Add(streetNumberColumn);
+
+            DataColumn zipCodeColumn = new DataColumn();
+            zipCodeColumn.DataType = typeof(int);
+            zipCodeColumn.ColumnName = "ZipCode";
+            Columns.Add(zipCodeColumn);
+
+            DataColumn buildYearColumn = new DataColumn();
+            buildYearColumn.DataType = typeof(int);
+            buildYearColumn.ColumnName = "BuildYear";
+            Columns.Add(buildYearColumn);
+
+            DataColumn squareMetersColumn = new DataColumn();
+            squareMetersColumn.DataType = typeof(int);
+            squareMetersColumn.ColumnName = "SquareMeters";
+            Columns.Add(squareMetersColumn);
+
+            DataColumn sellerIdColumn = new DataColumn();
+            sellerIdColumn.DataType = typeof(int);
+            sellerIdColumn.ColumnName = "SellerID";
+            Columns.Add(sellerIdColumn);
+
+            DataColumn priceColumn = new DataColumn();
+            priceColumn.DataType = typeof(int);
+            priceColumn.ColumnName = "Price";
+            Columns.Add(priceColumn);
+
+            DataColumn realtorIdColumn = new DataColumn();
+            realtorIdColumn.DataType = typeof(int);
+            realtorIdColumn.ColumnName = "RealtorID";
+            Columns.Add(realtorIdColumn);
+
+            DataColumn dateListedColumn = new DataColumn();
+            dateListedColumn.DataType = typeof(DateTime);
+            dateListedColumn.ColumnName = "DateListed";
+            Columns.Add(dateListedColumn);
+
+            DataColumn dateSoldColumn = new DataColumn();
+            dateSoldColumn.DataType = typeof(DateTime);
+            dateSoldColumn.ColumnName = "DateSold";
+            Columns.Add(dateSoldColumn);
+
+            DataColumn soldColumn = new DataColumn();
+            soldColumn.DataType = typeof(bool);
+            soldColumn.ColumnName = "Sold";
+            Columns.Add(soldColumn);
+
+            DataColumn sqmPriceColumn = new DataColumn();
+            sqmPriceColumn.DataType = typeof(int);
+            sqmPriceColumn.ColumnName = "SqmPrice";
+            Columns.Add(sqmPriceColumn);
+
+            DataColumn saelgerColumn = new DataColumn();
+            saelgerColumn.DataType = typeof(string);
+            saelgerColumn.ColumnName = "Sælger";
+            Columns.Add(saelgerColumn);
+        }
+    }
+}
---
+++ Models/DefaultSellersDataTable.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+
+namespace Models
+{
+    public class DefaultSellersDataTable : DataTable
+    {
+        public DefaultSellersDataTable()
+        {
+            TableName = "Sellers";
+
+            DataColumn idColumn = new DataColumn();
+            idColumn.DataType = typeof(int);
+            idColumn.ColumnName = "Id";
+            Columns.Add(idColumn);
+
+            DataColumn firstNameColumn = new DataColumn();
+            firstNameColumn.DataType = typeof(string);
+            firstNameColumn.ColumnName = "FirstName";
+            Columns.Add(firstNameColumn);
+
+            DataColumn lastNameColumn = new DataColumn();
+            lastNameColumn.DataType = typeof(string);
+            lastNameColumn.ColumnName = "LastName";
+            Columns.Add(lastNameColumn);
+
+            DataColumn cprNumberColumn = new DataColumn();
+            cprNumberColumn.DataType = typeof(string);
+            cprNumberColumn.ColumnName = "CprNumber";
+            Columns.Add(cprNumberColumn);
+
+            DataColumn streetNameColumn = new DataColumn();
+            streetNameColumn.DataType = typeof(string);
+            streetNameColumn.ColumnName = "StreetName";
+            Columns.Add(streetNameColumn);
+
+            DataColumn streetNumberColumn = new DataColumn();
+            streetNumberColumn.DataType = typeof(int);
+            streetNumberColumn.ColumnName = "StreetNumber";
+            Columns.Add(streetNumberColumn);
+
+            DataColumn zipCodeColumn = new DataColumn();
+            zipCodeColumn.DataType = typeof(int);
+            zipCodeColumn.ColumnName = "ZipCode";
+            Columns.Add(zipCodeColumn);
+
+            DataColumn phoneNumberColumn = new DataColumn();
+            phoneNumberColumn.DataType = typeof(string);
+            phoneNumberColumn.ColumnName = "PhoneNumber";
+            Columns.Add(phoneNumberColumn);
+        }
+    }
+}
